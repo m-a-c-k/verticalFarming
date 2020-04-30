e35543a59100b2eaa49e78bf8ff4d3b3adb85d87
mack <mackstarnes@gmail.com>
2020-04-30 18:01:05 -0500

    Update float_switch.ino

--- arduino/float_switch.ino
+++ arduino/float_switch.ino
@@ -1,1 +1,25 @@
-// float switch code
+// float switch demo code
+#include <stdint.h>
+
+// the number of the float switch pin connection
+#define FLOAT_SENSOR  2     
+
+void setup() 
+{
+ Serial.begin(115200);
+ // initialize the pushbutton pin as an input:
+ pinMode(FLOAT_SENSOR, INPUT_PULLUP);
+}
+
+void loop() 
+{
+  if(digitalRead(FLOAT_SENSOR) == LOW) 
+  {
+    Serial.print("Switch is Down");
+  } 
+  
+  else 
+  {
+    Serial.print("Switch is Up");
+  }
+}
